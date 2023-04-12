fb3ab35ddcc4d10eeac126735549f708b2368af0
Garima Sharma <80245962+Garima-Sharma18@users.noreply.github.com>
2023-04-12 16:07:03 +0530

    Create Cube.cpp

---
+++ Beginner/Cube.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int side,area,volume;
+	cin>>side;
+	area = 6*side*side;
+	volume = side*side*side;
+	cout<<area<<" "<<volume;
+	return 0;
+}
