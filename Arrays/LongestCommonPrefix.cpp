f7fd7c3c762cef8dc10efe14279c1749228da81f
Garima Sharma <80245962+Garima-Sharma18@users.noreply.github.com>
2023-07-15 00:07:06 +0530

    Create LongestCommonPrefix.cpp
    Using substr function of the string library we created a program to identify the longest common prefix amongst a vector of  strings

---
+++ Arrays/LongestCommonPrefix.cpp
@@ -0,0 +1,12 @@
+string longestCommonPrefix(vector<string> &str) {
+	if (str.size()==0)
+		return "";
+	string ans=str[0];
+	for (int i=1;i<str.size();i++)
+	{
+		while (str[i].find(ans)!=0)
+			ans = ans.substr(0,ans.length()-1);
+	}
+	return ans;
+	
+}
